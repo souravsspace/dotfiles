15192f3e4cc98d4e784673b3e9ae9a6afa479bc9
Sourav <souravsspace@gmail.com>
2025-05-10 23:34:53 +0600

    basic tmux and ghosty update

--- nvim/lua/plugins/ui/colorscheme.lua
+++ nvim/lua/plugins/ui/colorscheme.lua
@@ -48,17 +48,4 @@ return {
       vim.cmd [[colorscheme catppuccin]]
     end,
   },
-  -- {
-  --   'scottmckendry/cyberdream.nvim',
-  --   lazy = false,
-  --   priority = 1000,
-  --   config = function()
-  --     require('cyberdream').setup {
-  --       transparent = true,
-  --       italic_comments = true,
-  --       borderless_telescope = false,
-  --     }
-  --     vim.cmd [[colorscheme cyberdream]]
-  --   end,
-  -- },
 }
