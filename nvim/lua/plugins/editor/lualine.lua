e822f39409b08c7ee91524246e67e6f7ee8a8c61
Sourav <souravsspace@gmail.com>
2025-05-11 23:53:06 +0600

    comment out- diagnostics and diff from lualine

--- nvim/lua/plugins/editor/lualine.lua
+++ nvim/lua/plugins/editor/lualine.lua
@@ -24,8 +24,8 @@ return {
           separator = { left = '', right = '' },
           padding = { left = 1, right = 1, top = 1, bottom = 1 },
         },
-        'diff',
-        'diagnostics',
+        -- 'diff',
+        -- 'diagnostics',
       },
       lualine_c = {
         {
